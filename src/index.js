5252102959169d25eeed3013fafe5eb1a88f7ebe
Vyacheslav Terskikh <troxa843@gmail.com>
2023-03-08 17:26:03 +0300

    Add game engine

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import readlineSync from 'readline-sync';
+
+// Game Settings
+const roundAmount = 3;
+
+// Game Engine
+const startEngine = (generateQA, gameRules) => {
+  let attempts = 0;
+  let userAnswer;
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!\n${gameRules}`);
+  while (attempts < roundAmount) {
+    const [question, correctAnswer] = generateQA();
+    console.log(`Question: ${question}`);
+    userAnswer = readlineSync.question('Your answer? ');
+    if (correctAnswer === userAnswer) {
+      console.log('Correct!');
+      attempts += 1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default startEngine;
