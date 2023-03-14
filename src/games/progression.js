2fec74eeb44a6532645cf8030c2b0377504fdb08
Vyacheslav Terskikh <troxa843@gmail.com>
2023-03-14 23:53:17 +0300

    Code refactor

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,9 +2,9 @@ import getRandomNumber from '../utils.js';
 
 export const gameRules = 'What number is missing in the progression?';
 
-const generateProgression = (start, step, elements) => {
+const generateProgression = (start, step, progressionLength) => {
   const progression = [];
-  for (let i = 0; i < elements; i += 1) {
+  for (let i = 0; i < progressionLength; i += 1) {
     progression.push(start + step * i);
   }
   return progression;
@@ -13,8 +13,8 @@ const generateProgression = (start, step, elements) => {
 export const generateQA = () => {
   const start = getRandomNumber(1, 100);
   const step = getRandomNumber(2, 30);
-  const elements = 10;
-  const progression = generateProgression(start, step, elements);
+  const progressionLength = 10;
+  const progression = generateProgression(start, step, progressionLength);
   const elementToRemove = getRandomNumber(0, progression.length - 1);
   const correctAnswer = String(progression.at(elementToRemove));
   progression[elementToRemove] = '..';
